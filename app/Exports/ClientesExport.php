c38299148071a306172ac38c76da5e570b9cd3a5
Alan Carabali <74608008+maxhandx7@users.noreply.github.com>
2026-01-23 15:38:16 -0500

    v1.7.0

--- app/Exports/ClientesExport.php
+++ app/Exports/ClientesExport.php
@@ -2,7 +2,9 @@
 
 namespace App\Exports;
 
+use App\Http\Controllers\DashboardController;
 use App\Models\User;
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
@@ -13,8 +15,21 @@ class ClientesExport implements FromCollection, WithHeadings
      */
     public function collection()
     {
+        $controller = app(DashboardController::class);
 
         $clients = auth()->user()->descendantsAndSelf()->depthFirst()->get();
+
+        $ciudad = $controller->getCityName($clients);
+        $ciudad = $ciudad->toArray();
+        foreach ($clients as $index => $client) {
+            $client->ciudad = $ciudad[$index];
+        }
+
+        $departamento = $controller->getDepName($clients);
+        $departamento = $departamento->toArray();
+        foreach ($clients as $index => $client) {
+            $client->departamento = $departamento[$index];
+        }
 
         $data = $clients->map(function ($client) {
             return [
@@ -22,15 +37,18 @@ class ClientesExport implements FromCollection, WithHeadings
                 'cedula' => $client->cedula,
                 'name' => $client->name,
                 'surname' => $client->surname,
+                'fecha_nacimiento' => $client->fecha_nacimiento,
+                'edad' => Carbon::parse($client->fecha_nacimiento)->age,
                 'email' => $client->email,
                 'telefono' => $client->telefono,
-                'direccion' => $client->direccion,
-                'ciudad' => $client->ciudad,
+                'pais' => 'Colombia',
                 'departamento' => $client->departamento,
-                'pais' => $client->pais,
+                'ciudad' => $client->ciudad,
+                'comuna' => $client->comuna,
                 'barrio' => $client->barrio,
+                'direccion' => $client->direccion,
                 'estado' => $client->estado,
-                'created_at' => $client->created_at->format('Y-m-d H:i:s'),
+                'created_at' => Carbon::parse($client->created_at)->translatedFormat('d \d\e F \d\e Y'),
             ];
         });
         return $data;
@@ -45,13 +63,16 @@ class ClientesExport implements FromCollection, WithHeadings
             'Cédula',
             'Nombre',
             'Apellido',
+            'Fecha de Nacimiento',
+            'Edad',
             'Correo',
             'Teléfono',
-            'Dirección',
-            'Ciudad',
-            'Departamento',
             'País',
+            'Departamento',
+            'Ciudad',
+            'Comuna',
             'Barrio',
+            'Dirección',
             'Estado',
             'Fecha de Registro',
         ];
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
         <div class="row flex-center min-vh-100 py-6">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Reset Password') }}</div>
+                    <div class="card-header">{{ __('Reestablecer Contraseña') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('password.update') }}">
@@ -15,7 +15,7 @@
 
                             <div class="row mb-3">
                                 <label for="email"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Correo electrónico') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email"
@@ -32,7 +32,7 @@
 
                             <div class="row mb-3">
                                 <label for="password"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Contraseña') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
@@ -49,7 +49,7 @@
 
                             <div class="row mb-3">
                                 <label for="password-confirm"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Confirmar Contraseña') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
@@ -60,7 +60,7 @@
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Reset Password') }}
+                                        {{ __('Restablecer contraseña') }}
                                     </button>
                                 </div>
                             </div>
